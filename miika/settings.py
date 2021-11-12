42d634b1250ddfa5597745143175403e45cf69d0
treborrakk <robcalderon71@gmail.com>
2021-11-12 22:47:18 +0800

    update setting for allowed_host into github

--- miika/settings.py
+++ miika/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'exhlfdat&vfum(-34*c2uroi(($ww(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://immense-shore-17080.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
