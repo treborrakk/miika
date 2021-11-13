57948aed275c77f3c75a418dbab081314ee7f325
treborrakk <robcalderon71@gmail.com>
2021-11-13 12:34:47 +0800

    update setting moved the dj_database_url in upper level

--- miika/settings.py
+++ miika/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,7 +160,8 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
