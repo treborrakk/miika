2aedd70baaefa795b7a94d879534492e94e493a2
treborrakk <robcalderon71@gmail.com>
2021-11-13 17:32:35 +0800

    change make migrations into github

--- miika/settings.py
+++ miika/settings.py
@@ -94,6 +94,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+# 'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'miika',
+#         'USER': 'mikamekanika',
+#         'PASSWORD': 'vabbatch21',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
 }
 
 
